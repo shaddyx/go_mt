841f48298d40cb7e366316d1a331c62472a557d1
Anatolii Yakushko <ayakushko@playtika.com>
2023-03-19 02:25:41 +0200

    [+] added unmarshaller

--- util/reflection/reflect.go
+++ util/reflection/reflect.go
@@ -1,6 +1,10 @@
 package reflection
 
-import "reflect"
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
 
 func IsPointer(s any) bool {
 	return reflect.TypeOf(s).Kind() == reflect.Pointer
@@ -19,4 +23,21 @@ func IsStructurePointer(s any) bool {
 func IsStringPointer(s any) bool {
 	return IsPointer(s) &&
 		reflect.TypeOf(s).Elem().Kind() == reflect.String
+}
+
+func UnmarshalTo(jsn string, output any) error {
+	bJsn := []byte(jsn)
+	var err error
+	if !IsPointer(output) {
+		return fmt.Errorf("type is not a pointer: %s", reflect.TypeOf(output))
+	} else if IsStringPointer(output) {
+		output = jsn
+	} else if IsStructurePointer(output) {
+		err = json.Unmarshal(bJsn, output)
+	} else if IsMapPointer(output) {
+		err = json.Unmarshal(bJsn, output)
+	} else {
+		err = fmt.Errorf("unknown output type: %s", reflect.TypeOf(output).Name())
+	}
+	return err
 }
